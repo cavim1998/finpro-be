f4640e9a064fb6d097f481511c7eb354b48d1eb3
cavim1998 <75786954+cavim1998@users.noreply.github.com>
2026-01-31 17:17:27 +0700

    feat: outlet

---
+++ src/modules/outlet/dto/create-outlet.dto.ts
@@ -0,0 +1,31 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsNumber,
+  IsLatitude,
+  IsLongitude,
+} from "class-validator";
+import { Type } from "class-transformer";
+
+export class CreateOutletDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  addressText: string;
+
+  @IsOptional()
+  @IsNumber()
+  @IsLatitude()
+  @Type(() => Number)
+  latitude?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsLongitude()
+  @Type(() => Number)
+  longitude?: number;
+}
---
+++ src/modules/outlet/outlet.service.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "../../../generated/prisma/client.js";
+import { ApiError } from "../../utils/api-error.js";
+import { CreateOutletDto } from "./dto/create-outlet.dto.js";
+import { UpdateOutletDto } from "./dto/update-outlet.dto.js";
+
+export class OutletService {
+  constructor(private prisma: PrismaClient) {}
+
+  getAllOutlets = async () => {
+    return await this.prisma.outlet.findMany({
+      where: { isActive: true },
+      orderBy: { createdAt: "desc" },
+      include: {
+        _count: { select: { staff: true } },
+      },
+    });
+  };
+
+  getOutletById = async (id: number) => {
+    const getOutlet = this.prisma.outlet.findUnique({
+      where: { id },
+      include: {
+        staff: true,
+      },
+    });
+
+    if (!getOutlet) throw new ApiError("Outlet not found", 404);
+
+    return getOutlet;
+  };
+
+  createOutlet = async (body: CreateOutletDto) => {
+    return await this.prisma.outlet.create({
+      data: {
+        name: body.name,
+        addressText: body.addressText,
+        latitude: body.latitude,
+        longitude: body.longitude,
+      },
+    });
+  };
+
+  updateOutlet = async (id: number, body: UpdateOutletDto) => {
+    await this.getOutletById(id);
+    return await this.prisma.outlet.update({
+      where: { id },
+      data: body,
+    });
+  };
+
+  deleteOutlet = async (id: number) => {
+    const outlet = await this.getOutletById(id);
+
+    if (outlet?.staff.length !== 0)
+      throw new ApiError("The outlet still has employees", 400);
+
+    return await this.prisma.outlet.delete({
+      where: { id },
+    });
+  };
+}
